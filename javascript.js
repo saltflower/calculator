d21d329d7982cd2caa1a2292706a4a5efcaf7114
saltflower <132307660+saltflower@users.noreply.github.com>
2024-12-08 19:37:11 -0800

    feat: Add html calculator

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 console.log("test")
 
+let a, operator, b;
+
 function add(a, b) {
     return a + b
 }
@@ -14,4 +16,13 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b
+}
+
+function operate(a, b, operation) {
+    switch(operation) {
+        case "add": return add(a, b);
+        case "subtract": return subtract(a, b);
+        case "multiply": return multiply(a, b);
+        case "divide": return divide(a, b);
+    }
 }
